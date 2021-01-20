771c7e3748bdef3f7693c2380e312c0133c6e1c0
Damian Czapiewski <damczap13245@gmail.com>
2021-01-20 14:14:31 +0000

    feat: divide registration into steps

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,23 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
+use App\Entity\PaymentDetails;
 use App\Entity\User;
-use App\Form\RegistrationFormType;
+use App\Form\AddressType;
+use App\Form\PaymentDetailsType;
+use App\Form\PersonalDetailsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
     /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
      * @Route("/", name="app_registration")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(): Response
+    {
+        /** @var User $user */
+        $user = $this->session->get('registeringUser');
+        $step = $this->getCurrentStep($user);
+
+        return $this->redirectToStep($step);
+    }
+
+    /**
+     * @Route("/step1", name="app_registration_step1")
+     */
+    public function step1(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createForm(PersonalDetailsType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -30,23 +57,112 @@ class RegistrationController extends AbstractController
                 )
             );
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $this->session->set('registeringUser', $user);
 
-            return $this->redirectToRoute('app_registration_success');
+            return $this->redirectToRoute('app_registration_step2');
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('registration/step.html.twig', [
             'registrationForm' => $form->createView(),
+            'step' => 1
         ]);
     }
 
     /**
-     * @Route("/success", name="app_registration_success")
+     * @Route("/step2", name="app_registration_step2")
+     */
+    public function step2(Request $request): Response
+    {
+        /** @var User $user */
+        $user = $this->session->get('registeringUser');
+
+        $this->redirectToPreviousStepIfIncompleteData($user, 2);
+
+        $address = new Address();
+        $form = $this->createForm(AddressType::class, $address);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setAddress($address);
+            $this->session->set('registeringUser', $user);
+
+            return $this->redirectToRoute('app_registration_step3');
+        }
+
+        return $this->render('registration/step.html.twig', [
+            'registrationForm' => $form->createView(),
+            'step' => 2
+        ]);
+    }
+
+    /**
+     * @Route("/step3", name="app_registration_step3")
+     */
+    public function step3(Request $request): Response
+    {
+        /** @var User $user */
+        $user = $this->session->get('registeringUser');
+
+        $this->redirectToPreviousStepIfIncompleteData($user, 3);
+
+        $paymentDetails = new PaymentDetails();
+        $form = $this->createForm(PaymentDetailsType::class, $paymentDetails);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPaymentDetails($paymentDetails);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_registration_step4');
+        }
+
+        return $this->render('registration/step.html.twig', [
+            'registrationForm' => $form->createView(),
+            'step' => 3
+        ]);
+    }
+
+    /**
+     * @Route("/success", name="app_registration_step4")
      */
     public function success(): Response
     {
+        $this->session->remove('registeringUser');
+
         return $this->render('registration/success.html.twig');
+    }
+
+    private function redirectToPreviousStepIfIncompleteData(?User $user, int $step)
+    {
+        $currentStep = $this->getCurrentStep($user);
+
+        if ($currentStep < $step) {
+            $this->redirectToStep($currentStep);
+        }
+    }
+
+    private function getCurrentStep(?User $user): int
+    {
+        if (!$user) {
+            return 1;
+        }
+
+        if (!$user->getAddress()) {
+            return 2;
+        }
+
+        if (!$user->getPaymentDetails()) {
+            return 3;
+        }
+
+        return 4;
+    }
+
+    private function redirectToStep(int $step): Response
+    {
+        return $this->redirectToRoute('app_registration_step' . $step);
     }
 }
--- src/Form/PersonalDetailsType.php
+++ src/Form/PersonalDetailsType.php
@@ -6,26 +6,19 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class RegistrationFormType extends AbstractType
+class PersonalDetailsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -42,6 +35,9 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('firstName')
+            ->add('lastName')
+            ->add('telephone')
         ;
     }
 
